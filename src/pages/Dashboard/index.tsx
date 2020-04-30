6f5b791fd7fbe176794bd4b3acf72aa0b63159d9
Patrik Oliveira <patrik.oliveira@rarolabs.com.br>
2020-04-30 15:53:59 -0300

    fix: Correção para teste de listagem de transações

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -125,6 +125,7 @@ const Dashboard: React.FC = () => {
                         transaction.type === `income` ? 'income' : 'outcome'
                       }
                     >
+                      {transaction.type === 'outcome' ? '- ' : ''}
                       {transaction.formattedValue}
                     </td>
                     <td>{transaction.category.title}</td>
